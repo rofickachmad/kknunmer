6911a213685d305f00eb313bc50e6c090564daf9
Ahmad Rofiqul Muslikh <rofickachmad@icloud.com>
2018-12-03 22:34:00 +0700

    //AddPublicFolder

---
+++ public/assets/themes/coolui/body.php
@@ -0,0 +1,5 @@
+<body class="animsition">
+    <div class="page-wrapper">
+
+    <?php require_once(THEMESPATH.themes()'footer.php'); ?>
+</body>
---
+++ public/assets/themes/coolui/content_wrapper.php
@@ -0,0 +1,13 @@
+<div class="page-container2">
+            <!-- HEADER DESKTOP-->
+            <?php //require_once((THEMESPATH.themes().'/header.php')); ?>
+            <?php //require_once((THEMESPATH.themes().'/breadcrumb.php')); ?>
+            <?php require_once((THEMESPATH.themes().'/sidebar2.php')); ?>
+            
+            <!-- END HEADER DESKTOP-->
+
+           
+
+           
+
+            <?php //require_once((THEMESPATH.themes().'/widget.php')); ?>
